56c0e5f05c47e65082ca4f872afb757cf02a8632
Kazi Habiba <72264916+kazihabiba201@users.noreply.github.com>
2023-11-26 03:48:56 +0600

    Create switch.dart

---
+++ dart/switch.dart
@@ -0,0 +1,31 @@
+main() {
+  int day = 9;
+
+  switch (day) {
+    case 1:
+      print('Spring');
+      break;
+    case 2:
+      print('Summer');
+      break;
+    case 3:
+      print('Winter');
+      break;
+    case 4:
+      print('Autumn');
+      break;
+    case 5:
+      print('Monsoon');
+      break;
+    case 6:
+      print('Pre Winter');
+      break;
+    case 7:
+    case 8:
+    case 9:
+      print('Windy!');
+      break;
+    default:
+      print('Wrong Season!');
+  }
+}
