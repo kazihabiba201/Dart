1d3c3e8728f17d8a7de57fdfa656c70d703e17cc
Kazi Habiba <72264916+kazihabiba201@users.noreply.github.com>
2023-11-26 19:05:52 +0600

    Update functions.dart

--- dart/functions.dart
+++ dart/functions.dart
@@ -1,3 +1,30 @@
-void main(){
-  
+main() {
+  welcome();
+
+  int cal = add(15, 5);
+  print(cal);
+
+  double cal1 = mul(12.5, 4.6);
+  print(cal1);
+
+  subtract(cal, 25);
+}
+
+void welcome() {
+  print('Hello');
+}
+
+int add(int x, int y) {
+  int total = x + y;
+  return total;
+}
+
+double mul(double a, double b) {
+  double total1 = a * b;
+  return total1;
+}
+
+void subtract(int a, int b) {
+  int sub = a - b;
+  print('total is $sub');
 }
